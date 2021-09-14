e6238b089799f09d6da49b3a1e4de1813265780a
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-09-14 12:50:05 +0300

    Add header and delete helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Content,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+} 
+body {
+    height: 100vh;
+    overflow: hidden;
+    font-family: sans-serif;
+}
+ul {
+    list-style-type: none;
+}
+li:hover {
+    cursor: pointer;
+    text-decoration: underline;
+}
+a {
+    text-decoration: none;
+    color: #000000DE;
+    font-size: 13px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <header class="header">
+        <ul>
+            <li>
+                <a href="https:mail.google.com">Gmail</a>    
+            </li>
+            <li>
+                <a href="https://images.google.com/">Images</a>    
+            </li>
+
+            <li>
+                <div class="dropdown-apps">
+                    <img src="../assets/google-apps.png" alt="google apps" class="appimg">
+                </div>
+            </li>
+            <li>
+                <img src="../assets/account-image.png" alt="account image" class="accimg">
+            </li>
+        </ul>
+    </header>
+</template>
+<script>
+</script>
+
+<style>
+.dropdown-apps:hover .dropdown-content-apps {
+    display: block;
+}
+
+.appimg { 
+    max-height: 32px;
+}
+
+.appimg:hover {
+    background-color: #dadce0;
+    border-radius: 50%;
+} 
+
+.accimg {
+    width: 32px;
+    height: 32px;
+}
+
+.accimg:hover {
+    background-color: #dadce0;
+    border-radius: 50%
+}
+
+.header {
+    display: flex;
+    justify-content: flex-end;
+    padding: 12px 0;
+}
+
+.header ul {
+    display: flex;
+    align-items:center;
+}
+
+.header ul img {
+    border-radius: 50%;
+}
+
+.header ul li {
+    margin: 0 12px;
+    display: flex;
+    align-items:center;
+    justify-content: space-evenly;
+}
+</style>
